8a87874ba5e379a39a0b794c96267d0f0eeade8a
SAMSONSHUKURU <shukuru1@tritons.iowacentral.edu>
2022-08-26 10:24:27 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,14 @@
+/**************
+Samson Shukuru
+C++ Fall 2022
+Due : Aug. 31, 2022
+Lab 1 Introduction to C++ Programming
+This is lab is a demo of my ability to work with an IDE and GitHub.
+****************/
+
+#include <iostream>
+int main()
+{
+	std::cout << "Hello World!" << " Sage Green";
+	return 0;
+}
